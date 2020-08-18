9aedbb8912cea7af1d4d27e62483b153eadc439d
TCMEOZEL <68001010+TCMEOZEL@users.noreply.github.com>
2020-08-18 11:06:12 +0300

    Add files via upload

---
+++ main/java/mainapp/repository/coffee_rep.java
@@ -0,0 +1,9 @@
+package mainapp.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import mainapp.products.CoffeesEntity;
+
+@Repository
+public interface coffee_rep extends JpaRepository<CoffeesEntity,Integer> {
+}
